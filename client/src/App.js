5bca2bf2490f307adaaa3c6a12a69136e997a313
Marco Lopez <lopezvalmarco@gmail.com>
2020-08-04 20:20:56 -0700

    Se sube proyecto

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+import Cards from './components/Cards/Cards'
+
+const App = () => {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+
+    }, [])
+
+    return (
+        <div>
+            <Cards></Cards>
+        </div>
+    )
+}
+
+export default App;
